c11316aff5a8117ce256f79901bec700f26cc2b7
Emre Sorkun <emresorkun@gmail.com>
2021-12-25 18:54:51 +0100

    nav bar styled and unused code removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,84 +1,20 @@
-
-
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-//import logo from "../images/logo.svg";
-export default class Navbar extends Component {
-  state = {
-    isOpen: false,
-  };
-  handleToggle = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  };
-  render() {
-    return (
-      <nav className="navbar">
-        <div className="nav-center">
-          <div className="nav-header">
-            <Link to="/">
-              {/* <img src={logo} style={{ height: "3%" }} alt="" /> */}
-            </Link>
-          </div>
-          <ul
-            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
-          >
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/works">Completed Jobs</Link>
-            </li>
-            <li>
-              <Link to="/bookings">Book your Time</Link>
-            </li>
-            <li>
-              <Link to="/contact-form">Contact Form</Link>
-            </li>
-            <li>
-              <Link to="/comments">Comments</Link>
-            </li>
-            <li>
-              <Link to="/admin">Carpenter Login</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default function Navibar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" variant="light">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="contact-form">Contact Form</Nav.Link>
+          <Nav.Link href="comments">Your Comments</Nav.Link>
+          <Nav.Link href="bookings">Book your Carpenter</Nav.Link>
+          <Nav.Link href="works">Completed Works</Nav.Link>
+          <Nav.Link href="admin">Carpenter Login</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
-{/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav> */}
