46f5734b2831b6385165310b2c7e3465b1d9d05d
Emre Sorkun <emresorkun@gmail.com>
2022-01-03 22:27:14 +0100

    works page responsive media queries added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,26 +5,20 @@ const Home = () => {
     <>
       <header>
         <div className="wrapper">
-          <span className="logo">This is My Story</span>
+          <span className="logo">Our Services</span>
         </div>
       </header>
       <div className="home-container">
         <div className="home-wrapper">
           <div className="home-introduction">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-            fugiat iusto fuga praesentium optio, eaque rerum! Provident
-            similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-            eius earum ut molestias architecto voluptate aliquam nihil, eveniet
-            aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur
-            error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias
-            eos sapiente officiis modi at sunt excepturi expedita sint? Sed
-            quibusdam recusandae alias error harum maxime adipisci amet laborum.
-            Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates
-            a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius
-            fugit doloribus tenetur
+            Tahtadan Dukkan is an Ankara based woodworking company that provides
+            mid to high end custom woodworking. We specialize in building solid
+            furniture and cabinetry using only the best materials and joinery
+            techniques. Custom kitchens cabinets, bathroom vanities, tables,
+            consoles and built-ins are our's specialties. 
+
+            We speak English and Turkish and we mainly serve to internatinaol community
+            in Ankara. Please get in contact with us for more details and our references. 
           </div>
           <div className="home-photo">
             <img
