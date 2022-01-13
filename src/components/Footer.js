b7fe22801d5f0381fb5e107fa4a53478395db9c7
Emre Sorkun <emresorkun@gmail.com>
2022-01-13 22:56:48 +0100

    some more foooter styling and remove of unused code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,28 +2,29 @@ import React from "react";
 import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 const Footer = () => (
-  <div className="footer">
-    <p>HERE COMES THE FOOTER CONTENT</p>
-    <Nav className="mr-auto">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-          <Link to="contact-form" className="nav-link">
-            Contact Form
-          </Link>
-          <Link to="comments" className="nav-link">
-            Your Comments
-          </Link>
-          <Link to="bookings" className="nav-link">
-            Book your Carpenter
-          </Link>
-          <Link to="works" className="nav-link">
-            Portfolio
-          </Link>
-          <Link to="admin" className="nav-link">
-            Admin
-          </Link>
-        </Nav>
+  <div className="w-responsive text-center mx-auto p-3 mt-2">
+    <div className="footer">
+      <Nav className="mr-auto">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="contact-form" className="nav-link">
+          Contact Form
+        </Link>
+        <Link to="comments" className="nav-link">
+          Your Comments
+        </Link>
+        <Link to="bookings" className="nav-link">
+          Book your Carpenter
+        </Link>
+        <Link to="works" className="nav-link">
+          Portfolio
+        </Link>
+        <Link to="admin" className="nav-link">
+          Admin
+        </Link>
+      </Nav>
+    </div>
   </div>
 );
 
