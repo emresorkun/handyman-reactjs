7702e108b88297bd054982ca88ae94c90b16bb79
Emre Sorkun <emresorkun@gmail.com>
2022-01-17 11:27:26 +0100

    footer: socials added and styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,37 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import {
+  InstagramOutlined,
+  FacebookOutlined,
+  WhatsAppOutlined,
+} from "@ant-design/icons";
 const Footer = () => (
   <div className="w-responsive text-center mx-auto p-3 mt-2">
     <div className="footer">
+      {/* <div className="social-header">
+        <h4>Social</h4>
+      </div> */}
       <Nav className="mr-auto">
         <Link to="/" className="nav-link">
-          Home
+          <WhatsAppOutlined
+            style={{ fontSize: "38px", color: "rgba(255,255,255,.55)" }}
+          />
         </Link>
         <Link to="contact-form" className="nav-link">
-          Contact Form
+          <InstagramOutlined
+            style={{ fontSize: "38px", color: "rgba(255,255,255,.55)" }}
+          />
         </Link>
         <Link to="comments" className="nav-link">
-          Your Comments
-        </Link>
-        <Link to="bookings" className="nav-link">
-          Book your Carpenter
-        </Link>
-        <Link to="works" className="nav-link">
-          Portfolio
-        </Link>
-        <Link to="admin" className="nav-link">
-          Admin
+          <FacebookOutlined
+            style={{ fontSize: "38px", color: "rgba(255,255,255,.55)" }}
+          />
         </Link>
       </Nav>
+      <div className="copyright">
+        <p>Copyright © 2022 Tahtadan Dukkan - All Rights Reserved.</p>
+      </div>
     </div>
   </div>
 );
