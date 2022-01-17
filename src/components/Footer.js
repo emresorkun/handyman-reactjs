5d410416e688f54b791c1bbd3abef4be87fc0aa4
Emre Sorkun <emresorkun@gmail.com>
2022-01-17 23:04:52 +0100

    carusel images resized and links to socials added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -8,29 +9,38 @@ import {
 } from "@ant-design/icons";
 const Footer = () => (
   <div className="footer">
-    
-  <div className="w-responsive text-center mx-auto p-3 mt-2">
-    
+    <div className="w-responsive text-center mx-auto p-3 mt-2">
       {/* <div className="social-header">
         <h4>Social</h4>
       </div> */}
       <Nav className="mr-auto">
-        <Link to="/" className="nav-link">
+        <a
+          target="_blank"
+          href="https://www.instagram.com/tahtadantukkan/"
+          className="nav-link"
+        >
           <WhatsAppOutlined
             style={{ fontSize: "38px", color: "rgba(255,255,255,.55)" }}
           />
-        </Link>
-        <Link to="contact-form" className="nav-link">
+        </a>
+        <a
+          target="_blank"
+          href="https://www.instagram.com/tahtadantukkan/"
+          className="nav-link"
+        >
           <InstagramOutlined
             style={{ fontSize: "38px", color: "rgba(255,255,255,.55)" }}
           />
-        </Link>
-        <Link to="comments" className="nav-link">
+        </a>
+        <a
+          target="_blank"
+          href="https://www.facebook.com/profile.php?id=100009095115029"
+          className="nav-link"
+        >
           <FacebookOutlined
             style={{ fontSize: "38px", color: "rgba(255,255,255,.55)" }}
           />
-        </Link>
-        
+        </a>
       </Nav>
       <div className="copyright">
         <p>Copyright © 2022 Tahtadan Dukkan - All Rights Reserved.</p>
